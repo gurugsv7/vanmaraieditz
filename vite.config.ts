a58d9a96adf563716f46d0d84f0c6cc41b33057b
Guru <gurugsv777@gmail.com>
2025-02-06 23:14:47 +0530

    Update homepage URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this from '/vanmaraieditz/' to '/'
+  base: '', // Change this to empty string
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
