73967e56f6dea2cff2e5e0861564a87ae6341ec8
Guru <gurugsv777@gmail.com>
2025-02-06 23:05:07 +0530

    Update homepage URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vanmaraieditz/",  // 👈 Required for GitHub Pages deployment
+  base: "/",
+  build: {
+    outDir: 'dist'
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
